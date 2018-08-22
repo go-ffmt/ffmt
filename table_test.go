7bb0d349ee97980eda642f75f7b67ed97aa7bc93
wzshiming <wzshiming@foxmail.com>
2018-08-22 10:12:34 +0800

    update test

--- table_test.go
+++ table_test.go
@@ -36,11 +36,11 @@ func TestTable2(t *testing.T) {
 		{"Na": "aaaa", "Ba": "1231231"},
 	}
 
-	out := `Na    Ba         
-1111  123123     
-1     1231233231 
-aaaa  1231231    
-aaa3a`
+	out := `Ba         Na    
+123123     1111  
+1231233231 1     
+1231231    aaaa  
+           aaa3a`
 
 	resu := strings.TrimSpace(strings.Join(FmtTable(ToTable(b[0], b[0], b[1], b[2], map[string]string{
 		"Na": "aaa3a",
